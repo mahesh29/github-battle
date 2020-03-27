5b9e14b65d10ca2edd484002e79bfff0f8e7e9d4
Mahesh Bhandarkar <mahesh.bhandarkar.inft@gmail.com>
2020-03-27 01:04:20 -0700

    feat(withHover,HOC): add withHover Higher Order Component

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import withHover from './withHover'
 
 // Styles for the Tooltips
 const styles = {
@@ -24,47 +25,18 @@ const styles = {
     }
   }
 
-export default class Tooltip extends React.Component {
-
-    constructor (props) {
-        super(props)
-
-        this.state = {
-            hovering: false
-        }
-
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = this.mouseOut.bind(this)
-    }
-
-    mouseOver() {
-        this.setState({
-            hovering: true
-        })
-    }
-    mouseOut() {
-        this.setState({
-            hovering: false
-        })
-    }
-
-    render() {
-        const {text, children} = this.props
-        const {hovering} = this.state
-
-        return (
-            <div
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                style={styles.container}
-                >
-                {hovering && <div style={styles.tooltip}>{text}</div>}
-                {children}
-            </div>
-        )
-    }
+function Tooltip ({text, children, isHoveredOn}) {
+    return (
+        <div style={styles.container}>
+            {isHoveredOn && <div style={styles.tooltip}>{text}</div>}
+            {children}
+        </div>
+    )
 }
 
 Tooltip.propTypes = {
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    isHoveredOn: PropTypes.bool.isRequired
 }
+
+export default withHover(Tooltip, 'isHoveredOn')
